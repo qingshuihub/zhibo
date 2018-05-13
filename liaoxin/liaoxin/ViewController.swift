683637ee437be599d90496bf06a8cf1f9907cd0b
ren yufang <qingshuiyingren@gmail.com>
2018-05-14 00:00:21 +0800

    test

---
+++ liaoxin/liaoxin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  liaoxin
+//
+//  Created by fang on 2018/5/13.
+//  Copyright © 2018年 fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
